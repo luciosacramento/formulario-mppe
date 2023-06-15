e12bd099104058c438bcdc53271fb84235ed8526
lucio.sacramento <lucio.sacramento@tcm.ba.gov.br>
2023-06-15 10:53:39 -0300

    Solicitação de Certidão

--- functions.js
+++ functions.js
@@ -1,97 +1,136 @@
-window.onload = function(){
+window.onload = function () {
 
-var radioButtons = document.querySelectorAll('input[name="identify"]');
-let elements = document.querySelectorAll('.div-show-hide-identificar');
-elements.forEach(function(element) {
-    element.style.display = 'none';
-});
-
-radioButtons.forEach(function(radioButton) {
-
-    if (radioButton.checked) { 
-        radioGroup = radioButton.closest('.radio-group-identify');
-        radioGroup.classList.add('checked');
-        const atual = document.querySelector("#" + radioButton.value);
-        atual.style.display = 'contents';
-    }
-})
-
-radioButtons.forEach(function(radioButton) {
-  radioButton.addEventListener('change', function() {
-    const radioGroup = radioButton.closest('.radio-group-identify');
-    const elements = document.querySelectorAll('.div-show-hide-identificar');
-    elements.forEach(function(element) {
-      element.style.display = 'none';
-    });
-    const atual = document.querySelector("#" + radioButton.value);
-    atual.style.display = 'contents';
-    radioGroup.classList.add('checked');
-
-    radioGroup.classList.add('checked');
-    radioButtons.forEach(function(radio) {
-      if (radio !== radioButton) {
-        radio.closest('.radio-group-identify').classList.remove('checked');
-      }else{
-        radio.closest('.radio-group-identify').classList.add('checked');
-      }
-    });
-  });
-});
-
-
-radioButtons = document.querySelectorAll('input[name="tipo-manifestacao"]');
-elements = document.querySelectorAll('.div-show-hide-form');
-elements.forEach(function(element) {
-    element.style.display = 'none';
-});
-
-radioButtons.forEach(function(radioButton) {
-
-    if (radioButton.checked) { 
-        const atual = document.querySelector("#" + radioButton.value);
-        atual.style.display = 'contents';
-    }
-})
-
-showAllIdentifys();
-
-radioButtons.forEach(function(radioButton) {
-    radioButton.addEventListener('change', function() {
-      showAllIdentifys();  
-      const elements = document.querySelectorAll('.div-show-hide-form');
-      elements.forEach(function(element) {
+    /* Inicializando forms:
+    INDENTIFICADA / SIGILOSA / ANONIMA */
+    var radioButtons = document.querySelectorAll('input[name="identify"]');
+    let elements = document.querySelectorAll('.div-show-hide-identificar');
+    elements.forEach(function (element) {
         element.style.display = 'none';
-      });
-      const atual = document.querySelector("#" + radioButton.value);
-      atual.style.display = 'contents';
-      if(radioButton.value === 'elogio'){
-        document.querySelector("input[value='anonima']").closest('.radio-group-identify').style.display = 'none';
-      }
     });
-  });
 
-  function showAllIdentifys(){
-    const identifys = document.querySelectorAll("input[name='identify']");
+    /* Inicializando boxes:
+    INDENTIFICADA / SIGILOSA / ANONIMA */
+    radioButtons.forEach(function (radioButton) {
 
-    identifys.forEach(function(element) {
-        element.closest('.radio-group-identify').style.display = 'table'
+        if (radioButton.checked) {
+            radioGroup = radioButton.closest('.radio-group-identify');
+            radioGroup.classList.add('checked');
+            const atual = document.querySelector("#" + radioButton.value);
+            atual.style.display = 'contents';
+        }
+    })
+
+    /* Acão show hide dox boxes
+    INDENTIFICADA / SIGILOSA / ANONIMA */
+    radioButtons.forEach(function (radioButton) {
+        radioButton.addEventListener('change', function () {
+            const radioGroup = radioButton.closest('.radio-group-identify');
+            const elements = document.querySelectorAll('.div-show-hide-identificar');
+            elements.forEach(function (element) {
+                element.style.display = 'none';
+            });
+            const atual = document.querySelector("#" + radioButton.value);
+            atual.style.display = 'contents';
+            var all = document.querySelectorAll("input[name='identify']");
+            all.forEach(function (element) {
+                element.closest('.radio-group-identify').classList.remove('checked');
+            });
+            radioButton.closest('.radio-group-identify').classList.add('checked');
+
+        });
     });
-  }
 
-  var elementsWithAlt = document.querySelectorAll('[alt]');
 
-  elementsWithAlt.forEach(function(element) {
-    var tooltip = document.createElement('span');
-    tooltip.classList.add('tooltiptext');
-    tooltip.innerHTML  = element.getAttribute('alt');
+    hideAllForm();
 
-    var tooltipWrapper = document.createElement('span');
-    tooltipWrapper.classList.add('tooltip');
-    tooltipWrapper.appendChild(element.cloneNode(true));
-    tooltipWrapper.appendChild(tooltip);
+    /*Tornando visível o formulario selecionado*/
+    radioButtons = document.querySelectorAll('input[name="tipo-manifestacao"]');
+    radioButtons.forEach(function (radioButton) {
+        if (radioButton.checked) {
+            const atual = document.querySelector("#" + radioButton.value);
+            atual.style.display = 'contents';
+            document.querySelector("#vai-identificar").style.display = 'contents';
 
-    element.parentNode.replaceChild(tooltipWrapper, element);
-  });
+            if (radioButton.value === "solicitacao-certidao") {
+                document.querySelector("#vai-identificar").style.display = 'none';
+                document.querySelector("#vai-identificar-form").style.display = 'none';
+            }
+            if (radioButton.value === 'elogio') {
+                document.querySelector("input[value='anonima']").closest('.radio-group-identify').style.display = 'none';
+            }
+            if (radioButton.value === 'solicitacao-certidao') {
+                document.querySelector("#vai-identificar").style.display = 'none';
+            } else {
+                const atual = document.querySelector("#" + radioButton.value);
+                atual.style.display = 'contents';
+            }
+        }
+
+
+    })
+
+    showAllIdentifys();
+
+    radioButtons.forEach(function (radioButton) {
+        radioButton.addEventListener('change', function () {
+            showAllIdentifys();
+            const elements = document.querySelectorAll('.div-show-hide-form');
+            elements.forEach(function (element) {
+                element.style.display = 'none';
+            });
+            document.querySelector("#vai-identificar").style.display = 'contents';
+            document.querySelector("#vai-identificar-form").style.display = 'contents';
+
+            if (radioButton.value === 'elogio') {
+                document.querySelector("input[value='anonima']").closest('.radio-group-identify').style.display = 'none';
+            }
+            if (radioButton.value === 'solicitacao-certidao') {
+                document.querySelector("#vai-identificar").style.display = 'none';
+                document.querySelector("#vai-identificar-form").style.display = 'none';
+            }
+            const atual = document.querySelector("#" + radioButton.value);
+            atual.style.display = 'contents';
+
+        });
+    });
+    /*Tornando visível todos os Boxes de identificação*/
+    function showAllIdentifys() {
+        const elements = document.querySelectorAll('.radio-group-identify');
+        elements.forEach(function (element) {
+            element.style.display = 'table';
+        });
+    }
+
+    /*Ocultando todos os formularios*/
+    function hideAllForm() {
+        elements = document.querySelectorAll('.div-show-hide-form');
+        elements.forEach(function (element) {
+            element.style.display = 'none';
+        });
+    }
+
+    /*Ocultando todos os formularios de identificação*/
+    function hideAllFormIdentify() {
+        elements = document.querySelectorAll('.div-show-hide');
+        elements.forEach(function (element) {
+            element.style.display = 'none';
+        });
+    }
+
+    var elementsWithAlt = document.querySelectorAll('[alt]');
+
+    elementsWithAlt.forEach(function (element) {
+        var tooltip = document.createElement('span');
+        tooltip.classList.add('tooltiptext');
+        tooltip.innerHTML = element.getAttribute('alt');
+
+        var tooltipWrapper = document.createElement('span');
+        tooltipWrapper.classList.add('tooltip');
+        tooltipWrapper.appendChild(element.cloneNode(true));
+        tooltipWrapper.appendChild(tooltip);
+
+        element.parentNode.replaceChild(tooltipWrapper, element);
+    });
 
 
 }
